a073138d96021f504380e99d17b280c99d7dc4b8
yangxingkang <yangxingkang_gz@163.com>
2018-05-07 23:52:30 +0800

    d

--- passenger-web/src/main/webapp/js/admin/carTrip.js
+++ passenger-web/src/main/webapp/js/admin/carTrip.js
@@ -114,7 +114,7 @@ layui.use(['table'], function () {
                 }, {
                     fixed: 'right',
                     title: '操作',
-                    minWidth: 163,
+                    minWidth: 240,
                     // width: 178,
                     align: 'center',
                     toolbar: '#tableBtn'
@@ -246,7 +246,7 @@ layui.use(['table'], function () {
                 content: $('#tableBox')
             });
         } else if (obj.event === 'toArrive') {
-            window.location.href = window.location.origin + window.location.pathname + '?carTripId=' + data.id;
+            window.location.href = window.location.origin + '/route/setCarTripId?carTripId=' + data.id;
         }
     });
 
